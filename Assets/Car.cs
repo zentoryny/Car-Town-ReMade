20438eeb3876bbc8d60eaf4ff512bfdc4ddd9331
Tuna <zentorynytv@gmail.com>
2023-02-05 16:54:07 +0300

    main

---
+++ Assets/Car.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavepointindex = 0;
+
+    void Start()
+    {
+        target = WayPoints.points[0];
+         
+    }
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if (wavepointindex >= WayPoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+        }
+        wavepointindex++;
+        target = WayPoints.points[wavepointindex];
+    }
+}
---
+++ Assets/CarSpawner.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarSpawner : MonoBehaviour
+{
+    public Transform CarPrefab;
+
+    public Transform spawnPoint;
+    public float timeBetweenCars = 10f;
+    private float countdown = 3f;
+
+    private int waveIndex = 1;
+
+    private void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenCars;
+        }
+
+        countdown -= Time.deltaTime;
+    }
+
+    
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(1.0f);
+        }
+
+        
+    }
+
+    void SpawnEnemy ()
+    {
+        Instantiate(CarPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
